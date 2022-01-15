17da14188995717ad9a0a21108aefe0769672633
jcbritobr <jcbritobr@gmail.com>
2022-01-15 18:14:16 -0300

    First commit

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ pub const CRYPTO_BOX_CURVE_25519XSALSA20POLY1305_NONCEBYTES: usize = 24;
 pub const CRYPTO_BOX_CURVE_25519XSALSA20POLY1305_ZEROBYTES: usize = 32;
 pub const CRYPTO_BOX_CURVE_25519XSALSA20POLY1305_BOXZEROBYTES: usize = 16;
 
-pub fn crypto_hash_sha256(message: &String) -> Option<String> {
+pub fn crypto_hash_sha256(message: &str) -> Option<Vec<u8>> {
     unsafe {
         let hash = [0u8; CRYPTO_HASH_SHA256_BYTES].as_mut_ptr();
         let _res = crypto_hash_sha256_ref(hash, message.as_ptr(), message.len());
@@ -14,13 +14,13 @@ pub fn crypto_hash_sha256(message: &String) -> Option<String> {
             return None;
         } else {
             let data = std::slice::from_raw_parts(hash, CRYPTO_HASH_SHA256_BYTES);
-            let encoded_hash = hex::encode(data);
-            Some(encoded_hash)
+            let owned_hash = data.to_owned();
+            Some(owned_hash)
         }
     }
 }
 
-pub fn crypto_hash_sha512(message: &String) -> Option<String> {
+pub fn crypto_hash_sha512(message: &str) -> Option<Vec<u8>> {
     unsafe {
         let hash = [0u8; CRYPTO_HASH_SHA512_BYTES].as_mut_ptr();
         let _res = crypto_hash_sha512_ref(hash, message.as_ptr(), message.len());
@@ -28,13 +28,13 @@ pub fn crypto_hash_sha512(message: &String) -> Option<String> {
             return None;
         } else {
             let data = std::slice::from_raw_parts(hash, CRYPTO_HASH_SHA512_BYTES);
-            let encoded_hash = hex::encode(data);
-            Some(encoded_hash)
+            let owned_hash = data.to_owned();
+            Some(owned_hash)
         }
     }
 }
 
-pub fn crypto_box_keypair() -> Option<(String, String)> {
+pub fn crypto_box_keypair() -> Option<(Vec<u8>, Vec<u8>)> {
     let public_key = [0u8; CRYPTO_BOX_CURVE_25519XSALSA20POLY1305_PUBLIC_KEY_BYTES].as_mut_ptr();
     let secret_key = [0u8; CRYPTO_BOX_CURVE_25519XSALSA20POLY1305_SECRET_KEY_BYTES].as_mut_ptr();
     unsafe {
@@ -50,16 +50,46 @@ pub fn crypto_box_keypair() -> Option<(String, String)> {
                 secret_key,
                 CRYPTO_BOX_CURVE_25519XSALSA20POLY1305_SECRET_KEY_BYTES,
             );
-            let encoded_hash_pk = hex::encode(hash_pk);
-            let encoded_hash_sk = hex::encode(hash_sk);
-            Some((encoded_hash_pk, encoded_hash_sk))
+            let owned_hash_pk = hash_pk.to_owned();
+            let owned_hash_sk = hash_sk.to_owned();
+            Some((owned_hash_pk, owned_hash_sk))
         }
     }
+}
+
+pub fn crypto_box(message: &str, nonce: Vec<u8>, pk: Vec<u8>, sk: Vec<u8>) -> Option<Vec<u8>> {
+    let mut owned_message_zeros = vec![0u8; CRYPTO_BOX_CURVE_25519XSALSA20POLY1305_ZEROBYTES];
+    owned_message_zeros.extend_from_slice(message.as_bytes());
+    let mut cypher_text =
+        vec![0u8; CRYPTO_BOX_CURVE_25519XSALSA20POLY1305_ZEROBYTES + message.len()];
+
+    unsafe {
+        let ret = crypto_box_curve25519xsalsa20poly1305_ref(
+            cypher_text.as_mut_ptr(),
+            owned_message_zeros.as_ptr(),
+            owned_message_zeros.len(),
+            nonce.as_ptr(),
+            pk.as_ptr(),
+            sk.as_ptr(),
+        );
+        if ret == -1 {
+            return None;
+        }
+    }
+    Some(cypher_text)
 }
 
 extern "C" {
     fn crypto_hash_sha256_ref(hash: *mut u8, message: *const u8, message_len: usize) -> i32;
     fn crypto_hash_sha512_ref(hash: *mut u8, message: *const u8, message_len: usize) -> i32;
+    fn crypto_box_curve25519xsalsa20poly1305_ref(
+        cypher_text: *mut u8,
+        message: *const u8,
+        message_length: usize,
+        nonce: *const u8,
+        public_key: *const u8,
+        secret_key: *const u8,
+    ) -> i32;
     fn crypto_box_curve25519xsalsa20poly1305_ref_keypair(
         public_key: *mut u8,
         secret_key: *mut u8,
@@ -69,7 +99,8 @@ extern "C" {
 #[cfg(test)]
 mod tests {
     use crate::{
-        crypto_box_keypair, crypto_hash_sha256, crypto_hash_sha512,
+        crypto_box, crypto_box_keypair, crypto_hash_sha256, crypto_hash_sha512,
+        CRYPTO_BOX_CURVE_25519XSALSA20POLY1305_NONCEBYTES,
         CRYPTO_BOX_CURVE_25519XSALSA20POLY1305_PUBLIC_KEY_BYTES,
         CRYPTO_BOX_CURVE_25519XSALSA20POLY1305_SECRET_KEY_BYTES, CRYPTO_HASH_SHA256_BYTES,
         CRYPTO_HASH_SHA512_BYTES,
@@ -79,26 +110,39 @@ mod tests {
     fn test_crypto_hash_sha256_ref() {
         let mut message = "The quick brown fox jumped over the lazy dog".to_string();
         let hex_encoded_hash = crypto_hash_sha256(&mut message).unwrap();
-        assert_eq!(CRYPTO_HASH_SHA256_BYTES * 2, hex_encoded_hash.len());
+        assert_eq!(CRYPTO_HASH_SHA256_BYTES, hex_encoded_hash.len());
     }
 
     #[test]
     fn test_crypto_hash_sha512_ref() {
-        let mut message_2 = "The quick brown fox jumped over the lazy dog".to_string();
-        let _hex_encoded_hash = crypto_hash_sha512(&mut message_2).unwrap();
-        assert_eq!(CRYPTO_HASH_SHA512_BYTES * 2, _hex_encoded_hash.len());
+        let mut message = "The quick brown fox jumped over the lazy dog".to_string();
+        let hex_encoded_hash = crypto_hash_sha512(&mut message).unwrap();
+        assert_eq!(CRYPTO_HASH_SHA512_BYTES, hex_encoded_hash.len());
     }
 
     #[test]
     fn test_crypto_box_keypair() {
-        let keys = crypto_box_keypair().unwrap();
+        let (pk, sk) = crypto_box_keypair().unwrap();
         assert_eq!(
-            CRYPTO_BOX_CURVE_25519XSALSA20POLY1305_PUBLIC_KEY_BYTES * 2,
-            keys.0.len()
+            CRYPTO_BOX_CURVE_25519XSALSA20POLY1305_PUBLIC_KEY_BYTES,
+            pk.len()
         );
         assert_eq!(
-            CRYPTO_BOX_CURVE_25519XSALSA20POLY1305_SECRET_KEY_BYTES * 2,
-            keys.1.len()
+            CRYPTO_BOX_CURVE_25519XSALSA20POLY1305_SECRET_KEY_BYTES,
+            sk.len()
+        );
+    }
+
+    #[test]
+    fn test_crypto_box() {
+        let (pk, sk) = crypto_box_keypair().unwrap();
+        let message = "The quick brown fox jumped over the lazy dog";
+        let nonce = Vec::<u8>::with_capacity(CRYPTO_BOX_CURVE_25519XSALSA20POLY1305_NONCEBYTES);
+        let cypher_text = crypto_box(message, nonce, pk, sk);
+        assert_ne!(None, cypher_text);
+        assert_eq!(
+            &[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            &cypher_text.unwrap()[..16]
         );
     }
 }
